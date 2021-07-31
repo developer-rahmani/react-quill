2d539958315f78f34ae5475edec4cac80997fb84
Mohammadreza_Rahmani <developer.rahmani@gmail.com>
2021-07-31 21:43:13 +0430

    handle data

--- src/components/Quill.js
+++ src/components/Quill.js
@@ -1,20 +1,32 @@
+import { useState } from 'react';
 import { useQuill } from 'react-quilljs';
 import 'quill/dist/quill.snow.css'; // Add css for snow theme
 import './../styles/quill.scss';
 
 function Quill() {
+    const [value, setValue] = useState('');
+
     const formats = ['header', 'image', 'underline', 'italic', 'bold', 'blockquote', 'code-block', 'list', 'bullet', 'direction', 'align', 'color', 'background'];
     const modules = {
         toolbar: [[{ header: [1, 2, 3, false] }, 'image', 'underline', 'italic', 'bold', 'blockquote', 'code-block', { list: 'ordered' }, { list: 'bullet' }, { direction: 'rtl' }, { align: ['right', '', 'center', 'justify'] }, { color: [] }, { background: [] }, 'clean']],
     };
-    const { quillRef } = useQuill({ formats, modules });
+    const { quill, quillRef } = useQuill({ formats, modules });
+
+    const handleData = () => {
+        if (quill) {
+            setValue(quill.getText());
+        }
+    };
+
+    // console.log(value);
 
     return (
-        <>
-            <div className="quill">
-                <div ref={quillRef} />
-            </div>
-        </>
+        <div className="quill">
+            <div ref={quillRef} />
+            <button className="save" onClick={() => handleData()}>
+                Save
+            </button>
+        </div>
     );
 }
 
